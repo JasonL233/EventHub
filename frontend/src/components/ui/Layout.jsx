c76590f8aedde18efa97599314af58d9ca05d140
Jason Lin <lin1jason8@outlook.com>
2025-02-13 02:26:25 -0800

    Setup Frontend structure
    1. Initialized frontend using Vite+React
    
    2. Added dependencies: chakra-ui for UI, react-router-dom for routing control, and react-icon for icons.
    
    3. Created Components/ui folder for Layout, navbar, and sidebar components.
    
    4. Create pages folder for storing web pages.
    
    5. Inside Layout.jsx, built basic main page structure with 3 containers: Navbar, Sidebar, and Main Content.
    
    The Navbar designated for logo and Search bar. The Sidebar designated for navigation through each pages. And the main content designated for displaying content for each pages.
    
    !!! What you need to do after merging into your repository:
    1. cd to frontend folder
    2. npm install
    3. npm run dev

---
+++ frontend/src/components/ui/Layout.jsx
@@ -0,0 +1,27 @@
+import { Box, Container, Flex, Input, Button, VStack } from "@chakra-ui/react";
+import { Link, Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar"
+import Navbar from "./Navbar"
+
+// Layout Component
+const Layout = () => {  // Arrow function that returns a React JSX structure
+  return (
+   <Flex direction="column" height="100vh" p={3}>
+    <Container maxW="100%" height="18%" bg="green.800" color="white">
+      <Navbar />
+    </Container>
+    
+    <Flex flex="1" overflowY="hideen">  
+      <Container maxW="17%" mt={2} mr={2} p={0}>
+        <Sidebar />
+      </Container>
+
+      <Container maxW="83%" bg="blue.800" mt={2} overflowY="auto">  
+        <Outlet />  
+      </Container>
+    </Flex>
+   </Flex> 
+  )
+};
+
+export default Layout;

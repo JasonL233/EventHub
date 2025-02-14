a5b5db34cd63aff45f49eac3ae92f04d757dacaf
GaussianDoge <zheng10plus@gmail.com>
2025-02-13 18:35:20 -0800

    Initialize Navigate bar, create page, and home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Box, Button, useColorModeValue} from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import PostPage from "./pages/PostPage";
+
 
 function App() {
   return (
-    <Box minH={"100vh"}>
-      {/* <Navbar /> */}
-      <Routers>
+    <Box minH={"100vh"} bg = {useColorModeValue("red.500", "gray.900")}>
+      <Navbar />
+      <Routes>
         <Route path="/" element = {<HomePage />} />
-        <Route path="/create" element = {<PostPage />} />
-      </Routers>
+        <Route path="/create" element = {<CreatePage />} />
+        <Route path="/post" element = {<PostPage />} />
+      </Routes>
     </Box>
   );
 }

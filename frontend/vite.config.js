1190eeb498e33143bbab17544cdb6a1b2ab15378
GaussianDoge <zheng10plus@gmail.com>
2025-02-15 18:20:10 -0800

    Update vite config from the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // set default address for /api
-  sever:{
+  server: {
     proxy:{
       "/api":{
-        target:"http://localhost:4000"
+        target:"http://localhost:4000" // whenever visit /api, this will prefix to it automaticly
       }
     }
   }

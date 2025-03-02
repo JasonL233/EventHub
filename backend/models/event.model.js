9bea5b1dda8c7e65b9cb99b9064d7025dc431502
GaussianCat <zheng10plus@gmail.com>
2025-03-01 17:29:54 -0800

    Rewrite LikeButton as AutoLikeButton Fix Typo in event.model.js Fix Errors of PostPage due to the change of event schema

--- backend/models/event.model.js
+++ backend/models/event.model.js
@@ -37,7 +37,7 @@ const eventSchema = new mongoose.Schema({
         userId: {
           type: mongoose.Schema.Types.ObjectId,
           ref: "user",
-          requred: true,
+          required: true,
         },
         comment: {
           type: String,
